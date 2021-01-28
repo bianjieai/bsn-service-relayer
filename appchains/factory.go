b01c81a12fe92e7bd550d1c0629133b955194cd3
secret <secret@bianjie.ai>
2021-01-28 11:44:56 +0800

    modify factory

--- appchains/factory.go
+++ appchains/factory.go
@@ -4,42 +4,72 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/spf13/viper"
-
-	"relayer/appchains/bcos"
-	"relayer/appchains/ethereum"
-	"relayer/appchains/fabric"
+	"relayer/appchains/fisco"
 	"relayer/core"
 	"relayer/store"
 )
 
 // AppChainFactory defines an application chain factory
 type AppChainFactory struct {
-	Config *viper.Viper // configuration context in which the application chain is built
-	Store  *store.Store // store for the application chain
+	Store *store.Store // store
 }
 
 // NewAppChainFactory constructs a new application chain factory
-func NewAppChainFactory(config *viper.Viper, store *store.Store) AppChainFactory {
-	return AppChainFactory{
-		Config: config,
-		Store:  store,
+func NewAppChainFactory(store *store.Store) *AppChainFactory {
+	return &AppChainFactory{
+		Store: store,
 	}
 }
 
-// Make builds an application chain according to the given chain name
-func (f AppChainFactory) Make(chainName string) (core.AppChainI, error) {
-	switch strings.ToLower(chainName) {
+// BuildAppChain implements AppChainFactoryI
+func (f *AppChainFactory) BuildAppChain(chainType string, chainParams []byte) (core.AppChainI, error) {
+	switch strings.ToLower(chainType) {
 	case "eth":
-		return ethereum.MakeEthChain(ethereum.NewConfig(f.Config)), nil
+		// return ethereum.MakeEthChain(ethereum.NewConfig(f.Config)), nil
+		return nil, nil
 
 	case "fabric":
-		return fabric.MakeFabricChain(fabric.NewConfig(f.Config)), nil
+		// return fabric.MakeFabricChain(fabric.NewConfig(f.Config)), nil
+		return nil, nil
 
-	case "bcos":
-		return bcos.MakeBCOSChain(bcos.NewConfig(f.Config), f.Store), nil
+	case "fisco":
+		return fisco.BuildFISCOChain(chainParams, f.Store)
 
 	default:
-		return nil, fmt.Errorf("application chain %s not supported", chainName)
+		return nil, fmt.Errorf("application chain %s not supported", chainType)
+	}
+}
+
+// GetChainID implements AppChainFactoryI
+func (f *AppChainFactory) GetChainID(chainType string, chainParams []byte) (chainID string, err error) {
+	switch strings.ToLower(chainType) {
+	case "eth":
+		return "", nil
+
+	case "fabric":
+		return "", nil
+
+	case "fisco":
+		return fisco.GetChainIDFromBytes(chainParams)
+
+	default:
+		return "", fmt.Errorf("application chain %s not supported", chainType)
+	}
+}
+
+// StoreBaseConfig implements AppChainFactoryI
+func (f *AppChainFactory) StoreBaseConfig(chainType string, baseConfig []byte) error {
+	switch strings.ToLower(chainType) {
+	case "eth":
+		return nil
+
+	case "fabric":
+		return nil
+
+	case "fisco":
+		return fisco.StoreBaseConfig(f.Store, baseConfig)
+
+	default:
+		return fmt.Errorf("application chain %s not supported", chainType)
 	}
 }
