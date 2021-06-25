64cf7e52c9bdee4d7c7425948cd50edf9b0c2c92
王迪 <wangdi@bianjie.ai>
2021-06-25 10:30:59 +0800

    update iServicCore contract

--- appchains/fisco/iservice/contracts/src/interfaces/iServiceInterface.sol
+++ appchains/fisco/iservice/contracts/src/interfaces/iServiceInterface.sol
@@ -32,15 +32,4 @@ interface iServiceInterface {
         string  _errMsg,
         string  _output
     ) external returns (bool);
-
-
-    /**
-     * @dev call service/contract in dest chain
-     * @param _requestID Request id
-     * @param _callData call data from source chain
-     */
-    function callService(
-        bytes32 _requestID,
-        bytes  _callData
-    ) external returns (bool);
 }
